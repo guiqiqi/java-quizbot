697394bffc87eec0b2eb1dc7819db58b67766c0b
Doge Gui <guiqiqi187@gmail.com>
2024-06-02 19:05:44 +0300

    Add default constructor for models
    For fixing conversion with BeanPropertiesRowMapper

--- src/main/java/quizbot/model/AnswerHistory.java
+++ src/main/java/quizbot/model/AnswerHistory.java
@@ -2,11 +2,13 @@ package quizbot.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class AnswerHistory {
     private Integer id;
     private Integer answerer;
--- src/main/java/quizbot/model/User.java
+++ src/main/java/quizbot/model/User.java
@@ -2,11 +2,13 @@ package quizbot.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private Integer id;
     private String telegram;
